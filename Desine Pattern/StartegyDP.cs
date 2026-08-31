9083d7f1d530946c5635ec8fde298c0408f490e2
amarmern <amarmca51@gmail.com>
2026-08-31 11:05:28 +0530

    added the strategy desine Pattern

--- Desine Pattern/StartegyDP.cs
+++ Desine Pattern/StartegyDP.cs
@@ -1,39 +1,56 @@
-// Strategy
-public interface IPaymentStrategy
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
 {
-    void Pay(decimal amount);
-}
-
-// Concrete Strategy 1
-public class CreditCardPayment : IPaymentStrategy
-{
-    public void Pay(decimal amount)
+    public class Program
     {
-        Console.WriteLine("Paid using Credit Card: " + amount);
-    }
-}
+        // 1. Product interface
+        public interface IPaymentStrategy
+        {
+            void Pay(decimal amount);
+        }
 
-// Concrete Strategy 2
-public class UPIPayment : IPaymentStrategy
-{
-    public void Pay(decimal amount)
-    {
-        Console.WriteLine("Paid using UPI: " + amount);
-    }
-}
+        // 2. Concreate products
+        public class CreditCardsPyment : IPaymentStrategy
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine("Paid {amount} using credit card");
+            }
+        }
 
-// Context
-public class PaymentService
-{
-    private IPaymentStrategy _paymentStrategy;
+        public class UPIPayment : IPaymentStrategy
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine($"Paid {amount} using UPI Payment");
+            }
+        }
 
-    public PaymentService(IPaymentStrategy paymentStrategy)
-    {
-        _paymentStrategy = paymentStrategy;
-    }
+        //3. Context class of startegy 
 
-    public void MakePayment(decimal amount)
-    {
-        _paymentStrategy.Pay(amount);
+        public class PaymentService
+        {
+            private IPaymentStrategy _paymentStrategy;
+
+            public PaymentService(IPaymentStrategy paymentStrategy)
+            {
+                _paymentStrategy = paymentStrategy;
+            }
+
+            public void MakePayment(decimal amount)
+            {
+                _paymentStrategy.Pay(amount);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            IPaymentStrategy startegy = new UPIPayment();
+            PaymentService paymentService = new PaymentService(startegy);
+            paymentService.MakePayment(5000);
+        }
     }
 }
