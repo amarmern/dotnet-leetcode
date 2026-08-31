66076b4093a4ef0e4305e61f7ec28e4c330e6e69
amarmern <amarmca51@gmail.com>
2026-08-31 11:45:09 +0530

    added Decorator desin Pattern

---
+++ Desine Pattern/DecoratorDP.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        // Component
+        public interface IPayment
+        {
+            void Pay(decimal amount);
+        }
+
+        // Concrete Component
+        public class Payment : IPayment
+        {
+            public void Pay(decimal amount)
+            {
+                Console.WriteLine("Payment processed: " + amount);
+            }
+        }
+
+        // Decorator
+        public class PaymentDecorator : IPayment
+        {
+            protected IPayment _payment;
+
+            public PaymentDecorator(IPayment payment)
+            {
+                _payment = payment;
+            }
+
+            public virtual void Pay(decimal amount)
+            {
+                _payment.Pay(amount);
+            }
+        }
+
+        // Concrete Decorator - Logging
+        public class LoggingPaymentDecorator : PaymentDecorator
+        {
+            public LoggingPaymentDecorator(IPayment payment)
+                : base(payment)
+            {
+            }
+
+            public override void Pay(decimal amount)
+            {
+                Console.WriteLine("Payment started");
+
+                _payment.Pay(amount);
+
+                Console.WriteLine("Payment completed");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            IPayment payment = new Payment();
+
+            payment = new LoggingPaymentDecorator(payment);
+
+            payment.Pay(1000);
+
+        }
+    }
+}
